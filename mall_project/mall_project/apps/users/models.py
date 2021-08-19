4d449322656bfa4a1e0c523dcb6a2fddbd00150c
AugustRush <augustrush923@163.com>
2021-08-19 11:43:21 +0800

    收货地址数据库模型

--- mall_project/mall_project/apps/users/models.py
+++ mall_project/mall_project/apps/users/models.py
@@ -4,12 +4,16 @@ from django.conf import settings
 from itsdangerous import TimedJSONWebSignatureSerializer as TJWSSerializer, BadData
 
 from verification import constants
+from mall_project.utils.models import BaseModel
+from areas.models import Area
 # Create your models here.
 
 
 class User(AbstractUser):
     mobile = models.CharField(max_length=11, unique=True, verbose_name="手机号")
     email_active = models.BooleanField(default=False, verbose_name="邮箱验证状态")
+    default_address = models.ForeignKey('Address', related_name="users", null=True, blank=True,
+                                        on_delete=models.SET_NULL, verbose_name="默认收货地址")
 
     class Meta:
         db_table = 'tb_users'
@@ -43,3 +47,23 @@ class User(AbstractUser):
             return
         else:
             return user
+
+
+class Address(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='address', verbose_name="用户")
+    title = models.CharField(max_length=20, verbose_name="地址名称")
+    receiver = models.CharField(max_length=20, verbose_name="收货人")
+    province = models.ForeignKey(Area, on_delete=models.PROTECT, related_name="province_address", verbose_name="省")
+    city = models.ForeignKey(Area, on_delete=models.PROTECT, related_name="city_address", verbose_name="市")
+    district = models.ForeignKey(Area, on_delete=models.PROTECT, related_name="district_address", verbose_name="区")
+    place = models.CharField(max_length=50, verbose_name="地址")
+    mobile = models.CharField(max_length=11, verbose_name="手机号")
+    tel = models.CharField(max_length=20, blank=True, null=True, default='', verbose_name="座机号码")
+    email = models.EmailField(null=True, blank=True, default='', verbose_name="邮箱")
+    is_delete = models.BooleanField(default=False, verbose_name="逻辑删除")
+    is_default = models.BooleanField(default=False, verbose_name='默认地址')
+
+    class Meta:
+        db_table = "tb_address"
+        verbose_name_plural = verbose_name = "用户地址"
+        ordering = ['-is_default', '-update_time']
