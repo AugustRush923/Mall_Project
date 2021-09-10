39939390f934745a4ac9a99620349aaf03dd8733
AugustRush <augustrush923@163.com>
2021-09-10 17:34:01 +0800

    购物车全选/全取消

--- mall_project/mall_project/apps/cart/urls.py
+++ mall_project/mall_project/apps/cart/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from cart.views import CartView, CartSelectAllView
 
 urlpatterns = [
-    url(r'cart/', CartView.as_view()),
-    url(r'cart/selection/', CartSelectAllView.as_view()),
+    url(r'^cart/$', CartView.as_view()),
+    url(r'^cart/selection/$', CartSelectAllView.as_view()),
 ]
